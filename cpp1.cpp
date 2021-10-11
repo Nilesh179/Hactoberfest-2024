047da33162bcd61009e68e8e844be15268088255
Nilesh <nileshsuthar179@gmail.com>
2021-10-11 16:00:07 +0530

    Update cpp1.cpp

--- cpp1.cpp
+++ cpp1.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n, i, m=0, flag=0;
-    cout << "Enter the Number to check Prime: ";
+    int i, n;
+    bool isPrime = true;
+
+    cout << "Enter a positive integer: ";
     cin >> n;
-    if(n<2) {
-        cout<<"Number is not Prime."<<endl;
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        isPrime = false;
     }
     else {
-        for(i = 2; i <= n/2; i++) {
-            if(n % i == 0) {
-                cout<<"Number is not Prime."<<endl;
-                flag=1;
+        for (i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrime = false;
                 break;
             }
         }
-        if (flag==0)
-            cout << "Number is Prime."<<endl;
     }
+    if (isPrime)
+        cout << n << " is a prime number";
+    else
+        cout << n << " is not a prime number";
 
     return 0;
 }
